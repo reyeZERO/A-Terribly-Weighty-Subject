6cea0e78280cdc3ff86cf2295404c4145610bdb6
reyeZERO <reyescadenaj28@gmail.com>
2025-01-27 11:29:53 -0600

    tws.cpp

--- tws.cpp
+++ tws.cpp
@@ -1,32 +1,41 @@
 #include <iostream>
+#include <limits>
 using namespace std;
 
-int main() {
+int main() {    
     double y;
-    cout << "Welcome to the Ounce Conversion Program!\n";
+    cout<<"Welcome to the Ounce Conversion Program!\n";
+    cin>> y;
+    while(true) {        
 
-    while (true) {
-        cout << "Please enter the number of ounces: ";
-        cin >> y;
-
-        if (cin.fail()) {
-            cout << "This is not a number.\n";
-            cout << "Please try again.\n";
-            cin.clear();                  // Clear the error flag
-            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignore invalid input
-        } else if (y < 0) {
-            cout << "This number is negative. Ounces cannot be negative.\n";
-            cout << "Please try again.\n";
-        } else {
-            cout << "Thank you! Converting ounces to pounds.\n";
-            cout << y << " oz is equivalent to " << static_cast<int>(y) / 16 << " lbs and " 
-                 << static_cast<int>(y) % 16 << " oz.\n";
-            cout << y << " oz is equivalent to " << y / 16 << " lbs.\n";
-            cout << "Thank you for using the OCP!\n";
-            break;
-        }
+        
+        if( y < 0) {
+        cout<<"This number is negative oz cannot be in negative. \n";
+        cout<<"Please try Again.\n";
+        cin>> y;
     }
+        else if(cin.fail()) {        
+        cout<<"This is no a number. \n";
+        cout<<"Please try Again.\n";
+        cin.clear();
+        cin.ignore(256,'\n');
+        cin>>y;
+    }
+        else  {
+            cout<<"Thank you! Converting ounces to pounds.\n";
+            cout<<"\n";
+            cout<<y<<" oz is equivalent to "<<static_cast<int>(y)/16<<" lbs and "<<static_cast<int>(y)%16<<" oz.\n";
+            cout<<y<<" oz is equivalent to "<<(y)/16<<" lbs.\n";
+            cout<<"Thank you for using the OCP!\n";
+            break;
+        } 
+        
+    }       
+  
+          
+           
 
     return 0;
+
 }
 
